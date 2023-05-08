62ded1d85f7ff8487b932ad0f2a2cebc5afdb40f
Niels Thuis <cnops_niels@hotmail.com>
2023-05-08 16:34:44 +0200

    initial commit

---
+++ index.js
@@ -0,0 +1,78 @@
+import dotenv from "dotenv";
+dotenv.config();
+import express from "express";
+import mongoose from "mongoose";
+
+const app = express();
+const port = process.env.PORT || 3000;
+const url = process.env.MONGODB_URL;
+
+mongoose
+  .connect(url, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    console.log("Connected to MongoDB!");
+  })
+  .catch((err) => {
+    console.log(err);
+  });
+
+const schema = new mongoose.Schema(
+  {
+    title: String,
+    body: String,
+    photo: String,
+    metadata: {},
+  },
+  { collection: "davidApi" }
+);
+
+const Post = mongoose.model("Post", schema);
+
+app.use(express.json());
+
+app.get("/api/getAll", async (req, res) => {
+  const posts = await Post.find();
+  res.json(posts);
+});
+
+app.post("/api/post", async (req, res) => {
+  const post = new Post(req.body);
+  await post.save();
+  res.json(post);
+});
+
+app.delete("/api/delete/:id", async (req, res) => {
+  const { id } = req.params;
+  try {
+    const post = await Post.findByIdAndDelete(id);
+    if (!post) {
+      res.status(404).json({ message: "Post not found" });
+      return;
+    }
+    res.json({ message: "Post deleted successfully" });
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.patch("/api/patch/:id", async (req, res) => {
+  const { id } = req.params;
+  const updates = req.body;
+  try {
+    const post = await Post.findByIdAndUpdate(id, updates, { new: true });
+    if (!post) {
+      res.status(404).json({ message: "Post not found" });
+      return;
+    }
+    res.json(post);
+  } catch (error) {
+    console.log(error);
+    res.status(500).json({ message: "Internal server error" });
+  }
+});
+
+app.listen(port, () => console.log(`Listening on port ${port}...`));
