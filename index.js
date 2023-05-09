30522d52b6e5e8e83a9045a669f4a922aafec051
Niels Cnops <cnops_niels@hotmail.com>
2023-05-09 10:58:30 +0200

    adding more cors stuff

--- index.js
+++ index.js
@@ -32,6 +32,12 @@ const schema = new mongoose.Schema(
 const Post = mongoose.model("Post", schema);
 
 app.use(express.json());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
 
 app.get("/", async (req, res) => {
   const posts = await Post.find();
