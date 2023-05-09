c7c51f39bb95135e3f0f0e362b4a3455ba3b6e96
Niels Cnops <cnops_niels@hotmail.com>
2023-05-09 13:06:53 +0200

    trying to fix cors issues

--- index.js
+++ index.js
@@ -8,14 +8,8 @@ import mongoose from "mongoose";
 const app = express();
 const port = process.env.PORT || 3000;
 const url = process.env.MONGODB_URL;
-app.use(cors());
+app.use(cors({ origin: "*" }));
 app.use(express.json());
-app.use((req, res, next) => {
-  res.setHeader("Access-Control-Allow-Origin", "*");
-  res.setHeader("Access-Control-Allow-Methods", "GET, POST, PATCH, DELETE");
-  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
-  next();
-});
 
 mongoose
   .connect(url, {
